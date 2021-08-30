ddfef0ce5f862a8ce30d56e53039691a8f999c87
igoreta <ivoronline@gmail.com>
2021-08-30 11:07:36 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_native_subset/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_query_native_subset/controllers/MyController.java
@@ -11,19 +11,73 @@ public class MyController {
   @Autowired PersonRepository personRepository;
 
   //================================================================
-  // SOME PROPERTIES AS STRING
+  // RETURN SINGLE PROPERTY
   //================================================================
-  @RequestMapping("SomePropertiesAsString")
-  String somePropertiesAsString() {
-    String string = personRepository.somePropertiesAsString();
-    return string;
+  // 20
+  @RequestMapping("ReturnSingleProperty")
+  Integer returnSingleProperty() {
+    Integer age = personRepository.returnSingleProperty();
+    return  age;
   }
+
   //================================================================
-  // SOME PROPERTIES AS JSON
+  // RETURN SOME PROPERTIES AS STRING
   //================================================================
-  @RequestMapping("SomePropertiesAsJSON")
-  String somePropertiesAsJSON() {
-    String json = personRepository.somePropertiesAsJSON();
+  // John,20
+  @RequestMapping("ReturnSomePropertiesAsString")
+  String returnSomePropertiesAsString() {
+    String nameAge = personRepository.returnSomePropertiesAsString();
+    return nameAge;
+  }
+
+  //================================================================
+  // RETURN SOME PROPERTIES AS OBJECT ARRAY
+  //================================================================
+  // ["John",20]
+  @RequestMapping("ReturnSomePropertiesAsObjectArray")
+  Object returnSomePropertiesAsObjectArray() {
+    Object[] properties = (Object[]) personRepository.returnSomePropertiesAsObjectArray();
+    String   name       = (String)   properties[0];
+    Integer  age        = (Integer)  properties[1];
+    System.out.println(name + ", " + age);
+    return properties;
+  }
+
+  //================================================================
+  // RETURN SOME PROPERTIES AS SINGLE JSON RECORD
+  //================================================================
+  // {"name":"John","age":20}
+  @RequestMapping("ReturnsOMEPropertiesAsSingleJSONRecord")
+  String returnsOMEPropertiesAsSingleJSONRecord() {
+    String json = personRepository.returnSomePropertiesAsSingleJSONRecord();
+    return json;
+  }
+
+  //================================================================
+  // RETURN SOME PROPERTIES AS JSON ARRAY
+  //================================================================
+  // [{"name":"John","age":20}, {"name":"John","age":21}]
+  @RequestMapping("ReturnSomePropertiesAsJSONArray")
+  String returnSomePropertiesAsJSON() {
+    String json = personRepository.returnSomePropertiesAsJSONArray();
+    return json;
+  }
+
+  //================================================================
+  // RETURN ALL PROPERTIES AS SINGLE JSON RECORD
+  //================================================================
+  @RequestMapping("ReturnAllPropertiesAsSingleJSONRecord")
+  String returnAllPropertiesAsSingleJSONRecord() {
+    String json = personRepository.returnAllPropertiesAsSingleJSONRecord();
+    return json;
+  }
+
+  //================================================================
+  // RETURN ALL PROPERTIES AS JSON ARRAY
+  //================================================================
+  @RequestMapping("ReturnAllPropertiesAsJSONArray")
+  String returnAllPropertiesAsJSON() {
+    String json = personRepository.returnAllPropertiesAsJSONArray();
     return json;
   }
 
--- src/main/java/com/ivoronline/springboot_db_query_native_subset/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_native_subset/repositories/PersonRepository.java
@@ -7,23 +7,74 @@ import org.springframework.data.repository.CrudRepository;
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
   //=======================================================================================
+  // RETURN SINGLE PROPERTY
+  //=======================================================================================
+  // 20
+  @Query(value = "SELECT age FROM PERSON WHERE NAME = 'John' AND AGE = 20", nativeQuery = true)
+  Integer returnSingleProperty();
+
+  //=======================================================================================
   // RETURN SOME PROPERTIES AS STRING
   //=======================================================================================
   // John,20
   @Query(value = "SELECT name, age FROM PERSON WHERE NAME = 'John' AND AGE = 20", nativeQuery = true)
-  String somePropertiesAsString();
+  String returnSomePropertiesAsString();
 
   //=======================================================================================
-  // RETURN SOME PROPERTIES AS JSON
+  // RETURN SOME PROPERTIES AS OBJECT ARRAY
   //=======================================================================================
-  //RETURNS STRING: John,20
+  // ["John",20]
+  @Query(value = "SELECT name, age FROM PERSON WHERE NAME = 'John' AND AGE = 20", nativeQuery = true)
+  Object returnSomePropertiesAsObjectArray();
+
+  //=======================================================================================
+  // RETURN SOME PROPERTIES AS SINGLE JSON RECORD
+  //=======================================================================================
+  // {"name":"John","age":20}
   @Query(
     value =
-      "SELECT CAST(COALESCE(json_agg(row_to_json(rules_info)), CAST('[]' AS json)) AS varchar) " +
-      "FROM   (SELECT name, age FROM PERSON WHERE NAME = 'John' AND AGE = 20) " +
-      "AS     rules_info",
+      "SELECT CAST(row_to_json(EXPRESSION) AS VARCHAR) " +
+      "FROM (SELECT name, age FROM PERSON WHERE NAME = 'John' AND AGE = 20) AS EXPRESSION",
     nativeQuery = true
   )
-  String somePropertiesAsJSON();
+  String returnSomePropertiesAsSingleJSONRecord();
+
+  //=======================================================================================
+  // RETURN SOME PROPERTIES AS JSON ARRAY
+  //=======================================================================================
+  // [{"name":"John","age":20}, {"name":"John","age":21}] Returns String that looks like JSON Array
+  // []                                                   Returns [] if no Records are found
+  @Query(
+    value =
+      "SELECT CAST(COALESCE(json_agg(EXPRESSION), '[]') AS varchar) " +
+      "FROM   (SELECT name, age FROM PERSON WHERE NAME = 'John') AS EXPRESSION",
+    nativeQuery = true
+  )
+  String returnSomePropertiesAsJSONArray();
+
+  //=======================================================================================
+  // RETURN ALL PROPERTIES AS SINGLE JSON RECORD
+  //=======================================================================================
+  // {"name":"John","age":20}
+  @Query(
+    value =
+      "SELECT CAST(row_to_json(EXPRESSION) AS VARCHAR)" +
+      "FROM PERSON AS EXPRESSION WHERE NAME = 'John' AND AGE = 20",
+    nativeQuery = true
+  )
+  String returnAllPropertiesAsSingleJSONRecord();
+
+  //=======================================================================================
+  // RETURN ALL PROPERTIES AS JSON ARRAY
+  //=======================================================================================
+  // Rreturns String that contains JSON Array: [{"name":"John","age":20}, {"name":"John","age":21}]
+  // Returns [] if no Records are found:       []
+  @Query(
+    value =
+      "SELECT CAST(COALESCE(json_agg(EXPRESSION), '[]') AS VARCHAR)" +
+      "FROM PERSON AS EXPRESSION WHERE name = 'John'",
+    nativeQuery = true
+  )
+  String returnAllPropertiesAsJSONArray();
 
 }
